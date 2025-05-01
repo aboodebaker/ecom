c4ecf08008aa4e9bf2daec73ceb7026d74d2e7c8
Yaseen Aboobaker <yaseenaboobaker@Yaseens-MacBook-Air.local>
2025-05-01 13:56:45 +0200

    testing

--- src/payload/globals/Footer.ts
+++ src/payload/globals/Footer.ts
@@ -10,7 +10,7 @@ export const Footer: GlobalConfig = {
   fields: [
     {
       name: 'copyright',
-      label: 'copyright',
+      label: 'Copyright',
       type: 'text',
       required: true,
     },
